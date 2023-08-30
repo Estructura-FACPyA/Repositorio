7ff37a9b193a58ce06ee10167e4936b4a1ee6350
Adolfo_1126 <142751753+Imadolfo26@users.noreply.github.com>
2023-08-29 20:13:51 -0600

    Add files via upload

---
+++ Tuplas.py
@@ -0,0 +1,11 @@
+# Crear una tupla
+frutas = ("manzana", "platano", "naranja", "uva", "pera")
+
+# Acceder a elementos de la tupla
+print("La primera fruta es:", frutas[0])
+print("La tercera fruta es:", frutas[2])
+
+# Recorrer la tupla con un bucle
+print("Lista de frutas:")
+for fruta in frutas:
+    print(fruta)
