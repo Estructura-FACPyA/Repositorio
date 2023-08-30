bd1e99e9cd00d0dfb799787930ac28e435fd1937
Adolfo_1126 <142751753+Imadolfo26@users.noreply.github.com>
2023-08-29 20:14:14 -0600

    Add files via upload

---
+++ Diccionario de datos.py
@@ -0,0 +1,19 @@
+#Crear un diccionario con informacion sobre una persona
+persona = {
+    "Nombre" : "Ana" ,
+    "Edad" : 18,
+    "Profesion" : "Ingeniera",
+    "Ciudad" : "Monterrey"
+}
+
+# Acceder a los valores del diccionario
+Nombre = persona["Nombre"]
+Edad = persona["Edad"]
+Profesion = persona["Profesion"]
+Ciudad = persona["Ciudad"]
+
+#Imprimir la informacion de la persona
+print("Nombre:", Nombre)
+print("Edad:", Edad)
+print("Profesion:", Profesion)
+print("Ciudad:", Ciudad)
