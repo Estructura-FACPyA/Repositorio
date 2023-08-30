cb2536d3d713ddf6950d7c71a47fcbc8414a6e19
Adolfo_1126 <142751753+Imadolfo26@users.noreply.github.com>
2023-08-29 20:13:02 -0600

    Add files via upload

---
+++ Lista (Frutas).py
@@ -0,0 +1,14 @@
+Lista_frutas = ['Manzana', 'Platano', 'Pera', 'Kiwi', 'Uva', 'Mango', 'Papaya', 'Durazno', 'Sandia', 'Melon']
+for Lista_fruta in Lista_frutas:
+    print(Lista_fruta)
+# Imprime:
+# Manzana
+# Platano
+# Pera
+# Kiwi
+# Uva
+# Mango
+# Papaya
+# Durazo
+# Sandia
+# Melon
